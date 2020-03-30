983efe8ec67039919743f08a8807cc401b250b94
kangdmitry <57730514+kangdmitry@users.noreply.github.com>
2020-03-30 22:27:14 +0500

    f

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+//ref: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_overlay
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+ 
